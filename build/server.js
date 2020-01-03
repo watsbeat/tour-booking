1bfa2145213936a2b4639aa587958640fde8de43
watsbeat <c.watsonbeaton@gmail.com>
2020-01-03 21:58:23 +1000

    change tsconfig target to es2017; redirect output to ./build

--- build/server.js
+++ build/server.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = express_1.default();
-app.get('/', function (req, res, next) {
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res, next) => {
     res.send('Meetups Service');
 });
-app.listen(process.env.PORT || 8091, function () {
+app.listen(process.env.PORT || 8091, () => {
     console.log('Server started...');
 });
