10b7c0b58fe29151406c1ece71e5f9f9dd2087e7
watsbeat <c.watsonbeaton@gmail.com>
2020-01-03 21:54:16 +1000

    initial commit

---
+++ server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Meetups Service');
+});
+
+app.listen(process.env.PORT || 8091, () => {
+    console.log('Server started...');
+});
