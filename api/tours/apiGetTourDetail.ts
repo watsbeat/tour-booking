d2628f0906e6aa4fdd9a7d77c85d0a3d457dad1d
watsbeat <c.watsonbeaton@gmail.com>
2020-01-25 13:06:47 +1000

    use TourDetail model for getting a tour

--- api/tours/apiGetTourDetail.ts
+++ api/tours/apiGetTourDetail.ts
@@ -1,11 +1,13 @@
 import { DataStore } from '../../data/data';
 import { RequestHandler } from 'express';
+import { TourDetail } from '../../model/shared/tourDetail';
 
 export const apiGetTourDetail: RequestHandler = (req, res, next) => {
     const tourID = req.params.id;
     const selectedTour = DataStore.tours.find(element => element.id == tourID);
     if (selectedTour) {
-        res.json(selectedTour);
+        const selectedReviews = DataStore.reviews.filter(item => item.tourID);
+        res.json(new TourDetail(selectedTour, selectedReviews));
     } else {
         res.json({ status: 'failed', message: 'Element not found' });
     }
--- build/api/tours/apiGetTourDetail.js
+++ build/api/tours/apiGetTourDetail.js
@@ -1,13 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("../../data/data");
+const tourDetail_1 = require("../../model/shared/tourDetail");
 exports.apiGetTourDetail = (req, res, next) => {
     const tourID = req.params.id;
-    const selectedTour = data_1.DataStore.tours.find((element) => element.id == tourID);
+    const selectedTour = data_1.DataStore.tours.find(element => element.id == tourID);
     if (selectedTour) {
-        res.json(selectedTour);
+        const selectedReviews = data_1.DataStore.reviews.filter(item => item.tourID);
+        res.json(new tourDetail_1.TourDetail(selectedTour, selectedReviews));
     }
     else {
-        res.json({ "status": "failed", "message": "Element not found" });
+        res.json({ status: 'failed', message: 'Element not found' });
     }
 };
